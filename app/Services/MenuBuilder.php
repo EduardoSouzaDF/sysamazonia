9a947f1da48430408d70510377db08f33dba81b0
Eduardo Souza <eduardosouza.msi@gmail.com>
2026-07-31 15:00:11 -0300

    feat: Enhance registration management and evaluation features
    - Updated CheckAdmin middleware to support multiple roles for access control.
    - Introduced RegistrationStatusEnum for better status management in registrations.
    - Added new Opinion and Score models to handle evaluations and scores for registrations.
    - Implemented evaluation criteria retrieval and scoring logic in the Registration model.
    - Enhanced User model with methods for evaluator role checks and registration access.
    - Updated MenuBuilder service to include evaluator-specific menu options.
    - Modified views to conditionally display information based on user roles (admin/evaluator).
    - Created migrations for opinions and scores tables to support new evaluation features.

--- app/Services/MenuBuilder.php
+++ app/Services/MenuBuilder.php
@@ -10,7 +10,7 @@ class MenuBuilder
     public static function getMenuStructure(): array
     {
         $user = Auth::user();
-        if(!$user){
+        if (! $user) {
             return [];
         }
 
@@ -23,6 +23,13 @@ class MenuBuilder
 
         if ($user->hasRole(Role::LEITOR)) {
             $menus = array_merge($menus, self::getLeitorMenu());
+        }
+
+        if ($user->hasRole(Role::COMISSAO)) {
+            if ($user->isEvaluator()) {
+                $menus = array_merge($menus, self::getAvaliadorMenu());
+            }
+
         }
 
         return $menus;
@@ -112,6 +119,26 @@ class MenuBuilder
         ];
     }
 
+    public static function getAvaliadorMenu(): array
+    {
+
+        $user = Auth::user();
+
+        return [
+            [
+                'heading' => 'Avalliadores',
+            ],
+
+            [
+                'title' => 'Avaliar Inscrições',
+                // 'icon' => 'ki-abstract-26',
+                'icon' => 'ki-scroll',
+                'route' => 'admin.registration.index',
+            ],
+
+        ];
+    }
+
     public static function getLeitorMenu(): array
     {
         return [
@@ -123,7 +150,6 @@ class MenuBuilder
                 'icon' => 'ki-abstract-45',
                 'route' => 'dashboard',
             ],
-
 
         ];
     }
